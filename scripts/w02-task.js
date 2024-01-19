1e43644621c73e0cc6e49111db42564aa7390484
jrgauntjr <gau20002@byui.edu>
2024-01-18 17:29:05 -0700

    Finished Week 2's tasks

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,27 +3,29 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+const fullName = 'Joseph Gaunt';
+let currentYear = 2024;
+let profilePicture = 'images/_MGL5316.JPG';
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+textContext = currentYear;
+imageElement.setAttribute('src', `${profilePicture}`);
+yearElement.innerHTML = `${currentYear}`;
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-
-
-
-
-
-
+let foods = ["potatoes", "kitkat", "spaghetti", "steak"];
+const secondFood = "pie";
+foods.push(secondFood);
+foodElement.innerHTML += `<br>${foods}`;
+foods.shift();
+foodElement.innerHTML += `<br>${foods}`;
+foods.pop();
+foodElement.innerHTML += `<br>${foods}`;
